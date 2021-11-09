9e74f21a6a177425e1d1617d532f733af3ff94b2
Spencer <spencer9661@gmail.com>
2021-11-09 13:50:10 -0500

    Added Views for scoreboard on main recyclerview item. Started adding vector files for teams.

--- app/src/main/java/com/example/liveplayerstats/playercomponents/PlayerListAdapter.kt
+++ app/src/main/java/com/example/liveplayerstats/playercomponents/PlayerListAdapter.kt
@@ -8,8 +8,10 @@ import androidx.recyclerview.widget.ListAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.liveplayerstats.R
 import com.example.liveplayerstats.boxscore.Boxscore
+import com.google.android.material.imageview.ShapeableImageView
 
 class PlayerListAdapter : ListAdapter<Pair<Player, Boxscore>, PlayerListAdapter.PlayerViewHolder>(PlayersComparator()) {
 
@@ -27,27 +29,22 @@ class PlayerListAdapter : ListAdapter<Pair<Player, Boxscore>, PlayerListAdapter.
 
     inner class PlayerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        private val playerName: TextView = itemView.findViewById(R.id.playerName)
+        private val pName: TextView = itemView.findViewById(R.id.pName)
+        private val pPic: ShapeableImageView = itemView.findViewById(R.id.pPic)
+        private val pTeam: TextView = itemView.findViewById(R.id.pTeam)
 
         fun bind(pair: Pair<Player, Boxscore>?) {
             if (pair != null) {
-                Log.d("hi", "here")
-                Log.d("hi", pair.first.name)
-                Log.d("hi", pair.second.toString())
-                if (pair.second != null) {
-                    Log.d("hi", "here2")
-                }
+                val p = pair.first
+                pName.text = p.name
+                val idString = p.id
+                Glide.with(itemView)
+                    .load("https://cdn.nba.com/headshots/nba/latest/260x190/${idString}.png").centerCrop().circleCrop()
+                    .into(pPic)
+                pTeam.text =  p.teamName
             } else {
                 Log.d("hi", "null")
             }
-
-            //this just test
-            if (pair != null) {
-                val text = "${pair.first.name} ${pair.second.basicGameData.hTeam} vs ${pair.second.basicGameData.vTeam}"
-                playerName.text = text
-            }
-
-
         }
     }
 
