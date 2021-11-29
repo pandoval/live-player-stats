2c574ca9e2fd1c883266635f15ebed467c68c094
Spencer <spencer9661@gmail.com>
2021-11-28 19:05:04 -0500

    Added viewbinding and basic structure for programatically adding table rows to boxscore.

--- app/src/main/java/com/example/liveplayerstats/gameinfoactivity/BoxScoreFragment.kt
+++ app/src/main/java/com/example/liveplayerstats/gameinfoactivity/BoxScoreFragment.kt
@@ -2,11 +2,18 @@ package com.example.liveplayerstats.gameinfoactivity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.liveplayerstats.R
+import com.example.liveplayerstats.boxscore.BoxScore
+import com.example.liveplayerstats.databinding.FragmentBoxscoreBinding
 
 private const val PLAYER_TEAM_ID = "teamId"
 
@@ -19,16 +26,38 @@ class BoxScoreFragment : Fragment() {
         arguments.let {
             teamId = it?.getString(PLAYER_TEAM_ID).toString()
         }
-
-        
     }
 
+    private fun addRows(boxscore: BoxScore, homeTeam: Boolean) {
+        val tableLayout: TableLayout = if (homeTeam) binding.hTableLayout else binding.vTableLayout
+        for (i in 0..15) {
+            val row = TableRow(context)
+            //row.layoutParams = TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT,
+                //TableRow.LayoutParams.WRAP_CONTENT)
+
+        }
+    }
+
+    private fun valueTV(text: String): TextView {
+        val tv = TextView(context)
+        tv.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+            resources.getDimension(R.dimen.boxscore_tv_width).toInt())
+        tv.text = text
+        tv.gravity = Gravity.CENTER
+        tv.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+        tv.textSize = resources.getDimension(R.dimen.stat_text_size)
+        return tv
+    }
+
+    private var _binding: FragmentBoxscoreBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_boxscore, container, false)
+        _binding = FragmentBoxscoreBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     companion object {
